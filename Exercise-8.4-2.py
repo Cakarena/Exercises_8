ad4be2c959ddaa276b5f8c052ab6ed574aadb664
Anderson-Karena <30563088+Cakarena@users.noreply.github.com>
2017-10-08 17:28:52 +1100

    Add files via upload

---
+++ Exercise-8.4-2.py
@@ -0,0 +1,30 @@
+##https://www.py4e.com/html3/08-lists, Exercise 2
+## Figure out which line of the above program is still not properly guarded.
+## See if you can construct a text file which causes the program to fail 
+##and then modify the program so that the line is properly guarded 
+##and test it to make sure it handles your new text file.
+
+## The problem is if the SECOND word is missing also, not just the first, it will throw up an IndexError.
+## Created a file called mbox-short2.txt I removed the second word from the first two lines i.e. Sat
+## It just prints out a bunch of Fri and Thur now.
+
+## THIS IS THE OLD FILE
+#fhand = open('mbox-short2.txt', 'r')
+#count = 0
+#for line in fhand:
+#   words = line.split()
+#   # print('Debug:', words)
+#   if len(words) == 0 : continue
+#   if words[0] != 'From' : continue
+#   print(words[2])
+
+##THIS IS THE NEW FILE 
+
+fhand = open('mbox-short2.txt', 'r')
+count = 0
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) == 0 or len(words) < 3: continue
+    if words[0] != 'From' : continue
+    print(words[2])
